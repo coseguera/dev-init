3f64d0d9c279b33afca7f48544d153261de22a34
Carlos Oseguera Guerrero <carloseg@microsoft.com>
2022-01-12 22:42:13 -0800

    add an init script

---
+++ init.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+while getopts u:k:f: flag
+do
+    case "${flag}" in
+        u) username=${OPTARG};;
+        k) sshPublicKeyPath=${OPTARG};;
+        f) sourceCloudInit=${OPTARG};;
+    esac
+done
+echo "Username: $username"
+echo "SSH Key path: $sshPublicKeyPath"
+echo "Source cloud-init: $sourceCloudInit"
+
+BASEDIR=$(dirname "$0")
+OUTDIR=$BASEDIR/out
+CLOUDINIT=$OUTDIR/cloud-init.config
+
+mkdir -p $OUTDIR
+
+echo "copying cloud-init.yaml to $CLOUDINIT to replace values"
+cp $sourceCloudInit $CLOUDINIT
+
+echo "replacing values in cloud-init.config"
+
+sed -i '' "s/{{user}}/$username/g" $CLOUDINIT
+
+SSHPUBKEY=$(<$sshPublicKeyPath)
+sed -i '' "s|{{sshPublicKey}}|$SSHPUBKEY|g" $CLOUDINIT
