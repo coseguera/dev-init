4f80d0219fdecee7bb76c7f35361ef5d2ee50065
Carlos Oseguera Guerrero <carloseg@microsoft.com>
2022-01-15 18:21:52 -0800

    replace files from scripts folder into cloud-init file

--- init.sh
+++ init.sh
@@ -32,4 +32,13 @@ sed -i '' "s/{{user}}/$username/g" $CLOUDINIT
 SSHPUBKEY=$(<$sshPublicKeyPath)
 sed -i '' "s|{{sshPublicKey}}|$SSHPUBKEY|g" $CLOUDINIT
 
+FILES="$BASEDIR/scripts/*"
+for f in $FILES
+do
+    echo "replacing $f file if found in cloud-init..."
+    FILENAME=$(basename $f)
+    BASE64CONTENT=$(base64 $f)
+    sed -i '' "s|{{$FILENAME}}|$BASE64CONTENT|g" $CLOUDINIT
+done
+
 echo "done!"
